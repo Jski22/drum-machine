4134efdc38c1526d5962e78e5cbd5b0f49ea8706
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-11 16:06:23 -0500

    Added two buttons and css to segment and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { Segment } from 'semantic-ui-react'
+import { Segment, Button} from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Segment inverted>This is a Card</Segment>
+      <Segment size='massive'>
+        <Button size='massive' inverted color='red'>Q</Button>
+        <Button size='massive' inverted color='red'>W</Button>
+      </Segment>
     </div>
   );
 }
