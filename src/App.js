b946d241c81e287d342c7580852a49822946a10b
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-11 22:55:52 -0500

    Added more buttons and grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import { Segment, Button} from 'semantic-ui-react'
+import { Container, Button, Grid } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Segment size='massive'>
-        <Button size='massive' inverted color='red'>Q</Button>
-        <Button size='massive' inverted color='red'>W</Button>
-      </Segment>
+      <Container>
+        <Grid>
+          <Grid.Row centered>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>Q</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>W</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>E</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>A</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>S</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>D</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>Z</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>X</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button size='massive' inverted color='red'>C</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     </div>
   );
 }
