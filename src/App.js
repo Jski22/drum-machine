90dd6622b780ee8a5a780f3d046f76b8f8c06d71
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-13 20:38:24 -0500

    Added sound file for second button and styled the placement of buttons

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
 import { Container, Button, Grid } from 'semantic-ui-react';
-import audio from './assets/Heater-1.mp3';
+import heater1 from './assets/Heater-1.mp3';
+import heater2 from './assets/Heater-2.mp3';
 import './App.css';
 import React from 'react';
 
 function App() {
 
   function playAudio() {
-    new Audio(audio).play();
+    new Audio(heater1).play();
+  }
+
+  function playAudio1() {
+    new Audio(heater2).play();
   }
 
   return (
     <div className="App">
       <Container>
         <Grid>
-          <Grid.Row centered>
-            <Grid.Column>
+          <Grid.Row>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red' onClick={playAudio}> 
                 Q
               </Button>
             </Grid.Column>
-            <Grid.Column>
-              <Button size='massive' inverted color='red'>W</Button>
+            <Grid.Column width='2'>
+              <Button size='massive' inverted color='red' onClick={playAudio1}>W</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>E</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>A</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>S</Button>
             </Grid.Column>
           </Grid.Row>
-          <Grid.Row centered>
-            <Grid.Column>
+          <Grid.Row>
+            <Grid.Column width='3'>
               <Button size='massive' inverted color='red'>D</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>Z</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>X</Button>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column width='2'>
               <Button size='massive' inverted color='red'>C</Button>
             </Grid.Column>
           </Grid.Row>
