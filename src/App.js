2b3fde664890604bfc894997a8ce9c1a3672c1fb
Justen Schilinski <justenschilinski34@hotmail.com>
2023-01-17 17:04:49 -0500

    Added audio element and playAudio function to all 9 buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Container, Button, Grid } from 'semantic-ui-react';
 import heater1 from './assets/Heater-1.mp3';
 import heater2 from './assets/Heater-2.mp3';
+import heater3 from './assets/Heater-3.mp3';
+import heater4 from './assets/Heater-4.mp3';
+import clap from './assets/Heater-6.mp3';
+import openHH from './assets/Dsc_Oh.mp3';
+import kicknhat from './assets/Kick_n_Hat.mp3';
+import kick from './assets/RP4_KICK_1.mp3';
+import closeHH from './assets/Cev_H2.mp3';
 import './App.css';
 import React from 'react';
 
@@ -9,10 +16,6 @@ function App() {
   function playAudio(str) {
     var audio = document.getElementById(str); 
     audio.play();
-  }
-
-  function playAudio1() {
-    new Audio(heater2).play();
   }
 
   return (
@@ -27,30 +30,54 @@ function App() {
               </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red' onClick={playAudio1}>W</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip1")}>
+                <audio id="clip1" src={heater2}></audio>
+                W
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>E</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip2")}>
+                <audio id="clip2" src={heater3}></audio>
+                E
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>A</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip3")}>
+                <audio id="clip3" src={heater4}></audio>
+                A
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>S</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip4")}>
+                <audio id="clip4" src={clap}></audio>
+                S
+              </Button>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width='3'>
-              <Button size='massive' inverted color='red'>D</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip5")}>
+                <audio id="clip5" src={openHH}></audio>
+                D
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>Z</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip6")}>
+                <audio id="clip6" src={kicknhat}></audio>
+                Z
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>X</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip7")}>
+                <audio id="clip7" src={kick}></audio>
+                X
+              </Button>
             </Grid.Column>
             <Grid.Column width='2'>
-              <Button size='massive' inverted color='red'>C</Button>
+              <Button size='massive' inverted color='red' onClick={ () => playAudio("clip8")}>
+                <audio id="clip8" src={closeHH}></audio>
+                C
+              </Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
